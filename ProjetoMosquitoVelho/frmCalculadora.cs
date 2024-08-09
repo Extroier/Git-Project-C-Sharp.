a24a525080acd832c9b5c79834a04fc1c8fa0103
Extroier <henriquegdsmessias@gmail.com>
2024-08-09 11:52:31 -0300

    projeto mosquito Velho

---
+++ ProjetoMosquitoVelho/frmCalculadora.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoMosquitoVelho
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //LimparCampos
+        public void LimparCampos()
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            lblResposta.Text = "";
+            rdbAdicionar.Checked = false;
+            rdbSubtrair.Checked = false;
+            rdbMultiplicar.Checked = false;
+            rdbDividir.Checked = false;
+            rdbPotencia.Checked = false;
+            txtValor1.Focus();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Valor1, Valor2, resp=0;
+
+            Valor1 = Convert.ToDouble (txtValor1.Text);
+            Valor2 = Convert.ToDouble (txtValor2.Text);
+            //instânciar o objecto/classe usar o objecto sem modificar sua estrutura. Primeiro se criar variavel de mesmo tipo de objecto,variavel com mesmo nome da classe.
+            Operacoes variaveloperacoes = new Operacoes();
+            if(rdbAdicionar.Checked)
+            resp = Operacoes.somar(Valor1, Valor2);
+            if (rdbSubtrair.Checked)
+                resp = variaveloperacoes.menos(Valor1, Valor2);
+            if (rdbMultiplicar.Checked)
+                resp = variaveloperacoes.vezes(Valor1, Valor2);
+            if (rdbDividir.Checked)
+            {
+                if (Valor2== 0)
+                {
+                    MessageBox.Show("Falha,divisão com 0 impossível", "SistemaABC Ensina",
+                    MessageBoxButtons.YesNoCancel, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button3);
+                    //execultar metodo limpar
+                    LimparCampos();
+                }
+                else
+            { 
+                    resp = variaveloperacoes.dividir(Valor1, Valor2);
+                 }
+
+            }
+
+            if (rdbPotencia.Checked)
+                resp = variaveloperacoes.potencia(Valor1, Valor2);
+            lblResposta.Text = resp.ToString();
+            if (rdbDividir.Checked)
+            {
+                if (Valor2 == 0)
+                {
+                    MessageBox.Show("Falha,divisão com 0 impossível", "SistemaABC Ensina",
+                    MessageBoxButtons.YesNoCancel, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button3);
+                    //execultar metodo limpar
+                    LimparCampos();
+                }
+            }
+        
+        private void rdbAdicionar_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdbSubtrair_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProjetoMosquitoVelho/frmLogin.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace ProjetoMosquitoVelho
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.PctImagem = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PctImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PctImagem
+            // 
+            this.PctImagem.ErrorImage = ((System.Drawing.Image)(resources.GetObject("PctImagem.ErrorImage")));
+            this.PctImagem.Image = ((System.Drawing.Image)(resources.GetObject("PctImagem.Image")));
+            this.PctImagem.InitialImage = ((System.Drawing.Image)(resources.GetObject("PctImagem.InitialImage")));
+            this.PctImagem.Location = new System.Drawing.Point(152, 117);
+            this.PctImagem.Name = "PctImagem";
+            this.PctImagem.Size = new System.Drawing.Size(249, 165);
+            this.PctImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PctImagem.TabIndex = 0;
+            this.PctImagem.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(409, 117);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuário";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(409, 180);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(56, 20);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txbUser
+            // 
+            this.txbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUser.Location = new System.Drawing.Point(407, 140);
+            this.txbUser.MaxLength = 25;
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(270, 26);
+            this.txbUser.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.txtSenha.Location = new System.Drawing.Point(407, 203);
+            this.txtSenha.MaxLength = 12;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = 'l';
+            this.txtSenha.Size = new System.Drawing.Size(270, 25);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(407, 254);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(85, 28);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "&Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(592, 254);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(85, 28);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sai&r";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txbUser);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.PctImagem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.Text = "EmpresaABC";
+            ((System.ComponentModel.ISupportInitialize)(this.PctImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PctImagem;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
